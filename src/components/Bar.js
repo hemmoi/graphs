f62b8014c58f4c842aef80a329fe9ac96271178e
halonki <kimmo.halonen@cinia.fi>
2019-02-07 09:32:34 +0200

    test adding div

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,14 +1,16 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import graphContainer from './GraphContainer';
-import {VictoryBar, VictoryChart, VictoryAxis, VictoryTheme} from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
 
 class Bar extends Component {
     render() {
-        const {data} = this.props;
+        const { data } = this.props;
         return (
-            <VictoryChart theme={VictoryTheme.material} domainPadding={5}>
-                <VictoryBar data={data} x={0} y={1}/>
-            </VictoryChart>
+            <div>
+                <VictoryChart theme={VictoryTheme.material} domainPadding={5}>
+                    <VictoryBar data={data} x={0} y={1} />
+                </VictoryChart>
+            </div>
         )
     }
 }
